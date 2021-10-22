8581c78760f703c9703b36a6a2f023c36e9d275e
Pete Marvin <40055959+petemarvin@users.noreply.github.com>
2021-10-22 17:12:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-   ob_start();
-   include('autofit.html');
-   $returned = ob_get_contents();
-   ob_end_clean();
-
-   echo $returned;
+   $url = 'http://PhantomJScloud.com/api/browser/v2/a-demo-key-with-low-quota-per-ip-address/';
+$payload = file_get_contents ( 'request.json' );
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'content' => $payload
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+var_dump($result);
 ?>
